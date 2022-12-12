54a1876c55a383e30507d2b56ca7f8beb3b6c7be
Julien Nagel <jnagel@jnagelNMD6R.vmware.com>
2022-12-12 15:56:38 +0100

    we are using spring boot 2.7.6

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String myMsg = "<b>Hello, we are using spring boot v2.6.0</b>";
+		String myMsg = "<b>Hello, we are using spring boot v2.7.6</b>";
 		System.out.println("This is an update. myMsg = " + myMsg);
 		String msgToDisplay = myMsg + "Greetings from Spring Boot + Tanzu!";
 		return  msgToDisplay;
