0b2d5e03edfbfe72f398054328ac4006ebcc1b7a
Julien Nagel <jnagel@jnagelNMD6R.vmware.com>
2022-12-12 17:46:17 +0100

    this is an update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,9 +8,13 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String myMsg = "<b>Hello, we are using spring boot v2.7.6</b>";
+		String myMsg = "We are using spring boot v2.7.6";
 		System.out.println("This is an update. myMsg = " + myMsg);
-		String msgToDisplay = myMsg + "Greetings from Spring Boot + Tanzu!";
+		String msgToDisplay = "<table>";
+		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 36;\">" + "Hello, this is a wonderful java web app!" + "</h1></td></tr>";
+		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 24;\">" + myMsg + "</h1></td></tr>";
+		msgToDisplay = msgToDisplay + "<tr><td><img src='img_girl.jpg' alt='Girl in a jacket' width='500' height='600'></td></tr>" 
+		msgToDisplay = msgToDisplay + "</table>";
 		return  msgToDisplay;
 	}
 
