b0fc64cb3d8c1554f0747501212c3bd2ffc7dba9
Julien Nagel <jnagel@jnagelNMD6R.vmware.com>
2022-12-13 11:26:22 +0100

    we are using spring boot 3.0.0

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String myMsg = "We are using spring boot v2.7.6";
+		String myMsg = "We are using spring boot v3.0.0";
 		System.out.println("This is an update. myMsg = " + myMsg);
 		String msgToDisplay = "<table>";
 		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 60;\">" + "Hello, this is a wonderful java web app!" + "</h1></td></tr>";
