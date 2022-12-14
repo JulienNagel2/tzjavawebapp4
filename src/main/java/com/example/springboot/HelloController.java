20e02f36b85aff202fedbac8a2140ad0950ad713
Julien Nagel <jnagel@jnagelNMD6R.vmware.com>
2022-12-14 10:10:00 +0100

    we are using spring boot 2.6.0

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,11 +8,19 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		String myMsg = "We are using spring boot v2.7.6";
+		String myMsg = "This app is using spring boot v2.6.0";
 		System.out.println("This is an update. myMsg = " + myMsg);
+		String msgToDisplay = getWebContent(myMsg);
+		return  msgToDisplay;
+	}
+
+	private String getWebContent(String pMsg) {
+		java.util.Date d = new java.util.Date();
+		java.text.SimpleDateFormat formatter = new java.text.SimpleDateFormat("yyyy/MM/dd HH:mm:ss:SSS");
+		String dateString = formatter.format(d);
 		String msgToDisplay = "<table>";
-		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 60;\">" + "Hello, this is a wonderful java web app!" + "</h1></td></tr>";
-		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 48;\">" + myMsg + "</h1></td></tr>";
+		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 48;\">" + "Hello, the date is: " + dateString + "</h1></td></tr>";
+		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 48;\">" + pMsg + "</h1></td></tr>";
 		msgToDisplay = msgToDisplay + "<tr><td><h1 style=\"font-size: 48;\">" + "The app is powered by Tanzu Application Platform" + "</h1></td></tr>";
 		msgToDisplay = msgToDisplay + "<tr><td><img src='https://github.com/JulienNagel2/tzjavawebapp4/raw/main/TAP.png' alt='TAP'></td></tr>"; 
 		msgToDisplay = msgToDisplay + "</table>";
